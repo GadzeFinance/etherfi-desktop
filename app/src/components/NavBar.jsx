5d5be6684d261c87de4a608725c652fcefb29373
Nicky Khorasani <nicky@gadze.finance>
2023-01-27 17:46:28 -0500

    add chakra ui

---
+++ app/src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import { Flex, Tab, TabList, Image, Center } from '@chakra-ui/react'
+import React from 'react'
+
+
+// import EtherFiLogo from '../../assets/geo-logo-colour-landscape@2x.png'
+import tabButtonStyle from '../styleClasses/tabButtonStyle'
+import EtherFiLogo from '../assets/geo-logo-colour-landscape.png'
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <Flex height={'54px'} mx='40px'>
+                <Center height={'full'} width={'200px'}>
+                    <Image src={EtherFiLogo} />
+                </Center>
+                <Center height={'full'} flex='auto'>
+                    <TabList color='white'>
+                        <Tab sx={tabButtonStyle}>Staker!</Tab>
+                        <Tab sx={tabButtonStyle}> Node Operator!</Tab>
+                    </TabList>
+                </Center>
+            </Flex>
+        </nav>
+    )
+}
+
+
+
+export default NavBar
--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
-
+import { ChakraProvider } from '@chakra-ui/react'
+import App from './App.jsx'
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
     <div>
-        Hello world REACCCTT
+    <ChakraProvider>
+        <App/> 
+    </ChakraProvider>
     </div>
 );
