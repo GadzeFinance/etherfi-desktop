e5e23de9d174fa1048b52eeaadb0a4e07072c88b
Nicky Khorasani <nicky@ether.fi>
2023-01-29 00:00:43 -0500

    Add basic IPC for public key generation

---
+++ app/electron/preload.js
@@ -0,0 +1,17 @@
+const {
+    ipcRenderer,
+    contextBridge
+} = require("electron");
+
+// Expose protected methods off of window (ie.
+// window.api.sendToA) in order to use ipcRenderer
+// without exposing the entire object
+contextBridge.exposeInMainWorld("api", {
+    reqPublicBidFile: function(bidSize, bidPrice){
+        ipcRenderer.send("req-public-bid-file", [bidSize, bidPrice]);
+    },
+    receiveBidFileInfo: function(func){
+        ipcRenderer.once("receive-public-bid-file", (event, ...args) => func(event, ...args));       
+    }
+});
+c
