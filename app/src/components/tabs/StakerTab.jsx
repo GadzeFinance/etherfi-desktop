c8db211bbe630e8d6c6a55573da922d8796d180f
ReposCollector <seongyun@postech.ac.kr>
2023-01-30 07:57:30 -0500

    - Improve 'UploadFile' to deal with the case uploading the multiple files - Set up UIs for Staker

--- app/src/components/tabs/StakerTab.jsx
+++ app/src/components/tabs/StakerTab.jsx
@@ -1,20 +1,87 @@
-import React, {useState}from 'react'
+import React, { useEffect, useState } from 'react';
 import { Box, Center, ScaleFade, Input } from '@chakra-ui/react'
 import raisedWidgetStyle from '../../styleClasses/raisedWidgetStyle'
 import UploadFile from '../UploadFile';
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
+
+
 
 const StakerTab = ({ tabIndex }) => {
+  const [numValidatorKeyFiles, setNumValidatorKeyFiles] = useState(0);
+  const [numDepositDataFiles, setNumDepositDataFiles] = useState(0);
+  const [stakerAddress, setStakerAddress] = React.useState('')
+  const [depositedStakes, setDepositedStakes] = useState([])
+  const [password, setPassword] = useState('');
 
-    const [numFiles, setNumFiles] = useState(0);
+  const handleStakerAddressChange = (event) => setStakerAddress(event.target.value)
+  const handlePasswordChange = (event) => setPassword(event.target.value)
+
+  const getDepositedStakes = () => {
+    // let stakes = Get the list of Stake structs such that 
+    // (stake.staker_address == stakerAddress) AND (stake.phase == DEPOSITED)
+    
+    // For Testing
+    let stakes = [...depositedStakes, "Stakes of " + stakerAddress];    
+
+    setDepositedStakes(stakes);
+  }
+
+  const generateKeys = () => {
+
+  }
+
+  const exportStakeRequest = () => {
+
+  }
+
+  const exportStakePrivate = () => {
+    
+  }
+
 
   return (
     <Center>
     <Box maxW={'800px'} sx={raisedWidgetStyle}>
         <ScaleFade initialScale={0.5} in={tabIndex === 0}>
-        <UploadFile notifyFileChange={setNumFiles}/>
-          <Center>
-            <Input placeholder="Wallet Address" />
-          </Center>
+        <Center>
+          <Input variant='filled' 
+            color="red.500"
+            value={stakerAddress} 
+            onChange={handleStakerAddressChange} 
+            placeholder="staker wallet address" 
+          />
+          <Button colorScheme='blue' onClick={getDepositedStakes}>Enter</Button>
+        </Center>
+
+        <Text color="red.500" fontSize='xl'> 
+          You have staked {32*depositedStakes.length} (= 32 * {depositedStakes.length}) ETH and can launch {depositedStakes.length} staking nodes!{"\n"}
+          Please, use the ETH CLI tool to generate the validator keys and deposit data and submit them. 
+        </Text>
+        
+        <Text color="red.500" fontSize='xl'> 1. Validator Key Files </Text>
+        <UploadFile notifyFileChange={setNumValidatorKeyFiles}/>
+
+        <Text color="red.500" fontSize='xl'> 2. Deposit Data File </Text>
+        <UploadFile notifyFileChange={setNumDepositDataFiles}/>
+
+        <Text color="red.500" fontSize='xl'> 3. Password used to generate the validator keys and deposit data</Text>
+        <Input variant='filled' 
+            color="red.500"
+            value={password} 
+            onChange={handlePasswordChange} 
+            placeholder="password" 
+          />
+
+        <Center>
+          <Button colorScheme='blue' align='center' onClick={generateKeys}>Generate Keys</Button>
+        </Center>
+
+        <Center>
+          <Button colorScheme='blue' align='center' onClick={generateKeys}>Export stakeRequest.json</Button>
+          <Button colorScheme='blue' align='center' onClick={generateKeys}>Export stakePrivate.json</Button>
+        </Center>
+
         </ScaleFade>
     </Box>
     </Center>
