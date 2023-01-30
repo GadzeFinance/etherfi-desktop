c05eff2fac6e383681ff04eabc146f07d2b0f708
Nicky Khorasani <nicky@ether.fi>
2023-01-30 15:17:55 -0500

    Fix build scripts

--- app/src/components/tabs/NodeOperatorTab.jsx
+++ app/src/components/tabs/NodeOperatorTab.jsx
@@ -1,41 +1,49 @@
 import React, {useState} from 'react';
-import { Box, Center, ScaleFade, Input, NumberInput, Button, Flex, NumberInputField, VStack } from '@chakra-ui/react';
-import raisedWidgetStyle from '../../styleClasses/raisedWidgetStyle';
+import { Box, Center, ScaleFade, Button, VStack } from '@chakra-ui/react';
+import raisedWidgetStyle from '../../styleClasses/widgetBoxStyle';
 import BidInput from '../BidInput';
+import UploadFile from '../UploadFile';
+import {validateUploadedFiles, SCHEMAS} from '../../utils/schemaValidations';
 
 
 const NodeOperatorTab = ({ tabIndex }) => {
     const [bidSize, setBidSize] = useState(1);
     const [bidPrice, setBidPrice] = useState(0.01);
+    const [encryptedValKeysFile, setEncryptedValKeysFile] = useState()
+    const [nodeOperatorKeysFile, setNodeOperatorKeysFile] = useState()
 
     const generateBidRequest = () => {
         // TODO: Validate the bid Size and Bid Price values
         window.api.receiveBidFileInfo((event, arg) => {
-            console.log("BACK HERE???")
+            // What should we get back here// if anything?
             console.log(arg)
         })
+        // Send request to backend to make the files
         window.api.reqPublicBidFile(bidSize, bidPrice);
-        // Send Request to backend to make the keys
-
-        // 
-
     }
+
+
 
 
   return (
     <Center>
-        <Box maxW={'800px'} sx={raisedWidgetStyle} bg="#2b2852">
-            
-            <ScaleFade initialScale={0.5} in={tabIndex === 1}>
+        <ScaleFade initialScale={0.5} in={tabIndex === 1}>
             <VStack
                 spacing={4}
                 align='stretch'
+            >
+            <Box maxW={'800px'} sx={raisedWidgetStyle} bg="#2b2852">
+                <VStack
+                    spacing={4}
+                    align='stretch'
                 >
                 <Box>
+                    <Center color="white">
+                        Step 1
+                    </Center>
                     <BidInput placeholder="Bid Size:" value={bidSize}
                             setter={setBidSize} MIN={1} MAX={100}
                             increment={1}
-                            preci
                     />
                 </Box>
                 <Box>
@@ -44,14 +52,50 @@ const NodeOperatorTab = ({ tabIndex }) => {
                             increment={0.01}
                     />
                 </Box>
-                <Box>
+                    <Box>
                     <Center>
                     <Button bg="green" color="white" onClick={generateBidRequest}>Generate Bid Request</Button>
                     </Center>
                 </Box>
+                </VStack>
+            </Box>
+            {/* STEP 2 */}
+            <Box maxW={'800px'} sx={raisedWidgetStyle} bg="#2b2852">
+                <VStack
+                    spacing={4}
+                    align='stretch'
+                >
+                <Box>
+                    <Center color="white">
+                        Step 2
+                    </Center>
+                    <UploadFile notifyFileChange={async (files) => {
+                                    if (files.length > 0) {
+                                        // setHasFile(true)
+                                        const result = await validateUploadedFiles(files, "ValidatorKeys", SCHEMAS.EncryptedValidatorKeys)
+                                        console.log(result)
+                                        console.log("RESULT!!")
+                                        // setValidationErrors(result.errors)
+                                    } else {
+                                        const x = 10
+                                        // setHasFile(false)
+                                        // setValidationErrors([])
+                                    }
+                                }}      
+                    />
+                </Box>
+                <Box>
+                <UploadFile notifyFileChange={(val) => console.log(val)}/>
+                </Box>
+                    <Box>
+                    <Center>
+                    <Button bg="green" color="white" onClick={null}>Decrypt Validator Keys</Button>
+                    </Center>
+                </Box>
+                </VStack>
+            </Box>
             </VStack>
-            </ScaleFade>
-        </Box>
+        </ScaleFade>
     </Center>
   )
 }
