269d70685946e2112e8f95c1f3f233994d837003
ReposCollector <seongyun@postech.ac.kr>
2023-01-31 12:40:01 -0500

    Implemented loading files, Improved UI

--- app/src/components/tabs/StakerTab.jsx
+++ app/src/components/tabs/StakerTab.jsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Center, ScaleFade, Input } from '@chakra-ui/react'
 import raisedWidgetStyle from '../../styleClasses/raisedWidgetStyle'
-import UploadFile from '../UploadFile';
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import { Text } from '@chakra-ui/react'
 
 
-
 const StakerTab = ({ tabIndex }) => {
-  const [numValidatorKeyFiles, setNumValidatorKeyFiles] = useState(0);
-  const [numDepositDataFiles, setNumDepositDataFiles] = useState(0);
   const [stakerAddress, setStakerAddress] = React.useState('')
   const [depositedStakes, setDepositedStakes] = useState([])
   const [password, setPassword] = useState('');
+  const [validatorKeyFilePaths, setValidatorKeyFilePaths] = useState([])
+  const [depositDataFilePath, setDepositDatafilePath] = useState('')
 
   const handleStakerAddressChange = (event) => setStakerAddress(event.target.value)
   const handlePasswordChange = (event) => setPassword(event.target.value)
@@ -27,23 +25,31 @@ const StakerTab = ({ tabIndex }) => {
     setDepositedStakes(stakes);
   }
 
-  const encryptKeys = () => {
-
+  const selectValidatorKeyFiles = () => {
+    window.api.receiveSelectedFilesPaths((event, files) => {
+      console.log(files)
+      setValidatorKeyFilePaths(files)
+    })
+    window.api.reqSelectFiles();    
   }
 
-  const exportStakeRequest = () => {
-
+  const selectDepositDataFile = () => {
+    window.api.receiveSelectedFilesPaths((event, files) => {
+      console.log(files)
+      setDepositDatafilePath(files[0])
+    })
+    window.api.reqSelectFiles();    
   }
 
-  const exportStakePrivate = () => {
-    
+  const buildStakerFile = () => {
+    window.api.reqBuildStakerFile(validatorKeyFilePaths, depositDataFilePath, password);
   }
-
 
   return (
     <Center>
     <Box maxW={'800px'} sx={raisedWidgetStyle}>
         <ScaleFade initialScale={0.5} in={tabIndex === 0}>
+        <Text color="red.500" fontSize='xl'> 1. See how many validators you can launch! </Text>
         <Center>
           <Input variant='filled' 
             color="red.500"
@@ -56,30 +62,30 @@ const StakerTab = ({ tabIndex }) => {
 
         <Text color="red.500" fontSize='xl'> 
           You have staked {32*depositedStakes.length} (= 32 * {depositedStakes.length}) ETH and can launch {depositedStakes.length} staking nodes!{"\n"}
-          Please, use the ETH CLI tool to generate the validator keys and deposit data and submit them. 
+          Please, use the ETH CLI tool to generate new {depositedStakes.length} validator keys and deposit data. 
         </Text>
         
-        <Text color="red.500" fontSize='xl'> 1. Validator Key Files </Text>
-        <UploadFile notifyFileChange={setNumValidatorKeyFiles}/>
+        <Text color="red.500" fontSize='xl'> 2. Input Validator Key Files </Text>
+        <Center>
+        <Button colorScheme='blue' align='center' onClick={selectValidatorKeyFiles}>Select Validator KeyStore File ("keystore-m_...") </Button>
+        </Center>
 
-        <Text color="red.500" fontSize='xl'> 2. Deposit Data File </Text>
-        <UploadFile notifyFileChange={setNumDepositDataFiles}/>
+        <Text color="red.500" fontSize='xl'> 3. Input Deposit Data File </Text>
+        <Center>
+        <Button colorScheme='blue' align='center' onClick={selectDepositDataFile}>Select DepositData Files ("deposit_data-...")</Button>
+        </Center>
 
-        <Text color="red.500" fontSize='xl'> 3. Password used to generate the validator keys and deposit data</Text>
+        <Text color="red.500" fontSize='xl'> 4. Input Password used to generate the validator keys and deposit data </Text>
         <Input variant='filled' 
             color="red.500"
             value={password} 
             onChange={handlePasswordChange} 
             placeholder="password" 
-          />
+        />
 
+        <Text color="red.500" fontSize='xl'> 5. Finally, Encrypt them securely!</Text>
         <Center>
-          <Button colorScheme='blue' align='center' onClick={encryptKeys}>Encrypt Keys</Button>
-        </Center>
-
-        <Center>
-          <Button colorScheme='blue' align='center' onClick={exportStakeRequest}>Export stakeRequest.json</Button>
-          <Button colorScheme='blue' align='center' onClick={exportStakePrivate}>Export stakePrivate.json</Button>
+          <Button colorScheme='blue' align='center' onClick={buildStakerFile}>Encrypt</Button>
         </Center>
 
         </ScaleFade>
