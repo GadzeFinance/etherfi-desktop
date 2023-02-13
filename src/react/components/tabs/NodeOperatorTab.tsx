0c8f518b8e5e58b15b42e75552928d428d0410a0
Nicky Khorasani <nicky@ether.fi>
2023-02-13 11:53:34 -0500

    remove unused code in nodeOp tab

--- src/react/components/tabs/NodeOperatorTab.tsx
+++ src/react/components/tabs/NodeOperatorTab.tsx
@@ -12,21 +12,8 @@ interface TabProps {
 const NodeOperatorTab: React.FC<TabProps> = ({ tabIndex }: TabProps) => {
     const [numKeys, setNumKeys] = useState(500);
     const [connectedWallet, setConnectedWallet] = useState("0x2Fc348E6505BA471EB21bFe7a50298fd1f02DBEA") // hardcoded for now
-    const [savePath, setSavePath] = useState<string>("")
     const [encryptedValKeysFile, setEncryptedValKeysFile] = useState()
     const [nodeOperatorKeysFile, setNodeOperatorKeysFile] = useState()
-
-    const generateKeys = () => {
-        // Send request to backend to make the public and private key files
-        console.log(`connectedWallet: ${connectedWallet}`)
-        window.api.reqGenNodeOperatorKeys(numKeys, connectedWallet);
-    }
-    const selectSavePath = () => {
-        window.api.receiveSelectedFolderPath((event: Electron.IpcMainEvent, path: string) => {
-          setSavePath(path)
-        })
-        window.api.reqSelectFolder();    
-      }
 
 
   return (
