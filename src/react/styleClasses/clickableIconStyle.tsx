4ceebf7c014f8494c70d0c03a6236fedd85bd12b
Nicky Khorasani <nicky@ether.fi>
2023-03-14 17:15:19 -0500

    fix iconslash

--- src/react/styleClasses/clickableIconStyle.tsx
+++ src/react/styleClasses/clickableIconStyle.tsx
@@ -1,17 +1,21 @@
 
 const clickableIconStyle = {
-    borderRadius: '20%',
+    borderRadius: '1000px',
+    boxShadow: '0 0 20px 0 grey.transparent',
+
     _hover: {
         color: 'white',
         boxShadow: '0 0 20px',
         shadowColor: 'grey.transparent',
-        bg: 'grey.transparent',
+        bg: 'rgba(255, 255, 255, 0.317)',
 
     },
-    _active: { boxShadow: '0 0 30px', shadowColor: 'grey.dark' },
+    _active: {
+        boxShadow: '0 0 30px',
+        shadowColor: 'grey.dark',
+        bg: 'rgba(255, 255, 255, 0.21)',
+    },
     color: 'blue.secondary',
 }
 
 export default clickableIconStyle
-
-
