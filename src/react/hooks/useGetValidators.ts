72b19ab3c54178121bf3debd7e83234d55f922be
Dhruv Vora <dbvora03@gmail.com>
2023-06-05 09:34:08 -0500

    use beacon index insted of protocol validator index

--- src/react/hooks/useGetValidators.ts
+++ src/react/hooks/useGetValidators.ts
@@ -1,45 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useToast } from '@chakra-ui/react';
-import axios from 'axios'
-
-const graphqlEndpoint = process.env.NODE_ENV === 'production'
-? "https://api.studio.thegraph.com/query/41778/etherfi-mainnet/0.0.3"
-: 'https://api.studio.thegraph.com/query/41778/etherfi-goerli/0.0.1';
-
-const queryEndpoint = process.env.NODE_ENV === 'production'
-? "https://etherfi.vercel.app/api/beaconChain/findOneCollated?pubkey="
-: "https://goerli.etherfi.vercel.app/api/beaconChain/findOneCollated?pubkey="
-
-const getBeaconIndex = async (validatorID: number) => {
-
-  const hexID = `0x${validatorID.toString(16)}`
-  const data = {
-    query: `{
-        validators(where: {id: "${hexID}"}) {
-          id
-          validatorPubKey
-        }
-      }`,
-  };
-
-  try {
-    const response = await axios.post(graphqlEndpoint, data);
-
-    if (response.status === 200) {
-        let pub = response.data.data.validators[0].validatorPubKey;
-        const queryURL = queryEndpoint + pub;
-        const newResp = await axios.post(queryURL);
-        return newResp.data.db.validatorIndex;
-    } else {
-        throw new Error(
-            "GraphQL request failed with status: " + response.status
-        );
-    }
-  } catch (error) {
-      throw new Error("GraphQL request failed: " + error.message);
-  }
-
-}
 
 export default function useGetValidators(confirmedAddress: string, password: string) {
   const [fetchedValidators, setFetchedValidators] = useState(null);
@@ -59,10 +19,10 @@ export default function useGetValidators(confirmedAddress: string, password: str
               errorMessage: string
             ) => {
               if (result === 0) {
-                resolve(Object.entries(JSON.parse(validators)).map(async ([key, value]: [string, any]) => ({
+                resolve(Object.entries(JSON.parse(validators)).map(([key, value]: [string, any]) => ({
                     validatorID: key,
                     fileData: JSON.stringify(JSON.parse(value.keystore)),
-                    beaconID: await getBeaconIndex(parseInt(key))
+                    beaconID: value.beaconID
                 })))
               } else {
                 reject(errorMessage);
