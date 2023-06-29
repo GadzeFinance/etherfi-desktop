83de0a99374c710d27f6eb7e7ba36ca1e7a79a1d
brettgrigsby <gbrett@gmail.com>
2023-06-29 10:54:54 -0600

    update util with correct urls

--- src/electron/utils/getDappUrl.js
+++ src/electron/utils/getDappUrl.js
@@ -1,7 +1,6 @@
 const dappUrl = process.env.NODE_ENV === 'production'
   ? 'https://mainnet.ether.fi'
-  // : 'https://goerli.etherfi.vercel.app'
-  : 'http://localhost:3000'
+  : 'https://goerli.etherfi.vercel.app'
 
 module.exports = {
   dappUrl,
